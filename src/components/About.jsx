a9f6c6d1e89444250c6b7af524351e393296772f
shreyash3128 <shreyashgurav317@gmail.com>
2026-08-27 12:52:44 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <section>
+            <h2>About Me</h2>
+
+            <p>
+                I am a Software Developer passionate about
+                building web applications.
+            </p>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+      <section>
+          <h1>Hi, I'm Shreyash</h1>
+          <p>Software Developer</p>
+          <button>View My Projects</button>
+          <h2>Welcome to my portfolio</h2>
+      </section>
+  )
+}
+
+export default Hero
